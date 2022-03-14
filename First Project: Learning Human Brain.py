c604fdf5281876ba0095f2ceb65e71cb6df5de3e
Daredw036 <101533025+Daredw036@users.noreply.github.com>
2022-03-13 23:00:38 -0400

    Update First Project: Learning Human Brain.py

--- First Project: Learning Human Brain.py
+++ First Project: Learning Human Brain.py
@@ -1,1 +1,12 @@
-
+from keras.layers.convolutional import Conv2D
+from keras.layers import Dense, Flatten
+from keras.optimizers import RMSprop
+from keras.models import Sequential
+from skimage.transform import resize
+from skimage.color import rgb2gray
+from collections import deque
+from keras import backend as K
+import tensorflow as tf
+import numpy as np
+import random
+import gym
